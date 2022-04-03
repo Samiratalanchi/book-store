b1b53a9a0a5b55cac280205f4494907387a5937a
samira talanchi <samira73talanchi@gmail.com>
2022-04-03 11:05:19 +0430

    add constructor

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import Cart from "./components/Cart";
 import './components/Cart.css'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { products: [] }
+  }
+
+  componentWillMount() {
+    fetch("http://localhost:8000/products/")
+    .then(res => res.json())
+    .then(data => this.setState({
+      products: data
+    }))
+  }
+
   render() {
     return(
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-8">
-            <Products />
+            <Products products={this.state.products} />
           </div>
           <div className="col-md-4">
             <Cart />
