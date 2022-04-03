abb155ef2a45860f7c42333aed8c0a655fb65ca8
samira talanchi <samira73talanchi@gmail.com>
2022-04-03 16:09:41 +0430

    add price calculator function

--- src/components/cart.js
+++ src/components/cart.js
@@ -8,8 +8,25 @@ class Cart extends React.Component {
             <h2>سبد خرید</h2>
             {
               cartItems.length === 0 ? "سبد خرید خالی است" :
+                <div>
+                تعداد محصولات سبد خرید:  {cartItems.length}
+                </div>
+            }
+            {
+              cartItems.length > 0 &&
               <div>
-                تعداد محصولات سبد خرید: {cartItems.length}
+                <ul>
+                  {cartItems.map(item =>
+                    <li>
+                      {item.title}
+                      <button className="btn btn-light">حذف محصول</button>
+                    </li>
+                  )}
+                </ul>
+                <p>
+                  مجموع:
+                  {cartItems.reduce((a,b) => a + b.price * b.count,0)} تومان
+                </p>
               </div>
             }
           </div>
